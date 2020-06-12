86f4a08bdf61b630cda0f1c50a5955fdf6da54fc
Paolo Broglio <paolobrogliodev@gmail.com>
2020-06-12 17:11:36 +0200

    add camera

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::fs::File;
 use std::io::Write;
 
+use crate::camera::Camera;
 use crate::hit::{HitRecord, Hittable, HittableStore};
 use crate::ppm::write_color;
 use crate::ray::Ray;
@@ -12,6 +13,7 @@ mod ppm;
 mod vec3;
 mod hit;
 mod sphere;
+mod camera;
 
 fn ray_color(ray: Ray, world: &HittableStore) -> Color {
     let hit_record = HitRecord::new_def();
@@ -25,35 +27,36 @@ fn ray_color(ray: Ray, world: &HittableStore) -> Color {
     };
 }
 
+fn random_f32() -> f32 {
+    rand::random::<f32>()
+}
+
 
 fn main() -> std::io::Result<()> {
     let aspect_ratio = 16.0 / 9.0_f32;
     let image_width = 384;
     let image_height = (image_width as f32 / aspect_ratio) as i32;
+    let samples_per_pixel = 100;
 
     let mut content = String::new();
     content.push_str(format!("P3\n{} {}\n255\n", image_width, image_height).as_str());
-
-    let viewport_height = 2.0_f32;
-    let viewport_width = aspect_ratio * viewport_height;
-    let focal_length = 1.0_f32;
-
-    let origin = Point3::new(0.0, 0.0, 0.0);
-    let horizontal = Vec3::new(viewport_width, 0.0, 0.0);
-    let vertical = Vec3::new(0.0, viewport_height, 0.0);
-    let lower_left_corner = origin - (horizontal / 2.0) - (vertical / 2.0) - Vec3::new(0.0, 0.0, focal_length);
 
     let mut world = HittableStore::new();
     world.store(Box::new(Sphere::new(Point3::new(0.0, -100.5, -1.0), 100.0)));
     world.store(Box::new(Sphere::new(Point3::new(0.0, 0.0, -1.0), 0.5)));
 
+    let camera = Camera::new();
 
     for j in 0..image_height {
         for i in 0..image_width {
-            let u = i as f32 / (image_width - 1) as f32;
-            let v = (image_height - 1 - j) as f32 / (image_height - 1) as f32;
-            let ray = Ray::new(origin, lower_left_corner + (horizontal * u) + (vertical * v) - origin);
-            content.push_str(write_color(ray_color(ray, &world)).as_str());
+            let mut pixel_color = Color::new(0.0, 0.0, 0.0);
+            for s in 0..samples_per_pixel {
+                let u = (i as f32 + random_f32()) / (image_width - 1) as f32;
+                let v = (image_height - 1 - j) as f32 + random_f32() / (image_height - 1) as f32;
+                let r = camera.get_ray(u, v);
+                pixel_color = pixel_color + ray_color(r, &world);
+            }
+            content.push_str(write_color(pixel_color, samples_per_pixel).as_str());
         }
     }
 
--- src/ppm.rs
+++ src/ppm.rs
@@ -1,8 +1,20 @@
 use crate::vec3::Color;
 
-pub fn write_color(color: Color) -> String {
-    let r = (color.x() * 255.999) as u32;
-    let g = (color.y() * 255.999) as u32;
-    let b = (color.z() * 255.999) as u32;
+fn clamp(x: f32, min: f32, max: f32) -> f32 {
+    if x < min {
+        return min;
+    }
+    if x > max {
+        return max;
+    }
+    return x;
+}
+
+
+pub fn write_color(color: Color, samples_per_pixel: i32) -> String {
+    let scale = 1.0 / samples_per_pixel as f32;
+    let r = clamp(color.x() * scale, 0.0, 0.999) as u32 * 256;
+    let g = clamp(color.y() * scale, 0.0, 0.999) as u32 * 256;
+    let b = clamp(color.z() * scale, 0.0, 0.999) as u32 * 256;
     format!("{} {} {}\n", r, g, b)
 }
